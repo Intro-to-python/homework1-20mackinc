5f37bd274304ad0d41d369e8e7cca44b053f4c14
Charlotte Mackin <cmackin@cshnyc.org>
2018-10-16 13:09:11 -0400

    hw1

---
+++ problem1.py
@@ -0,0 +1,26 @@
+#homework1
+#Due 10/10/18
+
+# Problem 1
+#Write a Python program to count the number
+#of even and odd numbers from a series of numbers.
+#Be sure to print out what numbers are even and what numbers are odd.
+#(Hint use a for loop)
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
+countOdd = 0
+countEven = 0
+for x in numbers:
+    if not x % 2:
+        countEven+=1
+    else:
+        countOdd+=1
+print("Number of even numbers :",countEven)
+print("Number of odd numbers: ",countOdd)
+
+#x = countOdd
+#y = countEven
+#for x in range(0,10,2):
+    #print(even)
+#for y in range(0,10):
+    #print(odd)
---
+++ problem2.py
@@ -0,0 +1,25 @@
+#homework1
+#Due 10/10/18
+
+# Problem 2
+#Write a Python program to guess a number between 1 to 9.
+# User is prompted to enter a guess. If the user guesses wrong then
+#the prompt appears again until the guess is correct, on successful guess,
+#user will get a "Well guessed!" message, and the program will exit.
+#(Hint use a while loop)
+#Don't forget to import random
+
+#targetNum = (1,9)
+#guessNum = input("Guess a number: ")
+#while x>0:
+    #print("Well guessed!")
+#while x<10:
+    #print("Well guessed!")
+#else:
+    #print(Sorry. Try again!)
+
+import random
+targetNum, guessNum = random.randint(1,10), 0
+while targetNum != guessNum:
+    guessNum = int(input("Guess a number between 1 and 10 until you get it right: "))
+print("Well guessed!")
